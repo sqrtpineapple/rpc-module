5d7c5041be7bcc32c1c000a9dd73bb47ecfa9a52
sqrtpineapple <xbj1997@foxmail.com>
2022-10-29 12:54:02 +0800

    Impl base classes

---
+++ rpc-client/core/include/RpcClient.hpp
@@ -0,0 +1,9 @@
+#include "RpcClientConfig.hpp"
+#include "rpc_client_common.hpp"
+
+class RpcClient {
+public:
+
+private:
+    RpcClientConfigPtr _config;
+};
---
+++ rpc-client/core/include/rpc_client_common.hpp
@@ -0,0 +1,5 @@
+#include "rpc_common_classes.hpp"
+
+POINTER_DECLARED(PeerInfo)
+POINTER_DECLARED(RpcClient)
+POINTER_DECLARED(RpcClientConfig)
